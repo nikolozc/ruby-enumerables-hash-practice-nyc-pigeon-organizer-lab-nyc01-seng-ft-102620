edc1998419b3e90a94e4ed77990c816e4e0fe0f2
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 04:52:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,6 +11,7 @@ def nyc_pigeon_organizer(data)
         if(!pigeon_list[name][key])
           pigeon_list[name][key] = []
         end 
+        pigeon_list[name][key]<<(key2)
       end 
     end 
   end 
