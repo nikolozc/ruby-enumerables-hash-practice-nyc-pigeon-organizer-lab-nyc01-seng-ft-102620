15b242fd6ad3737d8a08c5f779a1704a2f99d4c6
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 04:42:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,22 +2,5 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   pigeon_list = {}
-  #Put all names in the hash 
-  data.each do |key, element|
-    key.each do |key2,element2|
-      if(!pigeon_list.includes?(element2))
-        pigeon_list<<(element2)
-      end 
-    end 
-  end 
-  
-  #add tier1 key,values
-  pigeon_list.each do |key|
-    data.each do |keyD,elementD|
-      key[keyD] = []
-    end 
-  end 
-  
-  #add values to tier1 keys
-  
+  hash.each_with_object
 end
