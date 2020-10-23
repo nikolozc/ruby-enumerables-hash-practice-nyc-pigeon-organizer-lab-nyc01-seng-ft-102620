7b213d60589a2dbbb02fa7e84b2fe5109d63268b
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 04:58:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   pigeon_list = {}
-  hash.each_with_object(pigeon_list) do |(key,element), pigeon_list|
+  hash.each_with_object({}) do |(key,element), pigeon_list|
     element.each do |key2, element2|
       element2.each do |name|
         if(!pigeon_list[name])
